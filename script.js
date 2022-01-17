fe1b0468eb0de61eaf50eb122ac947bb41d7dbeb
ekucukkirli <56414732+ekucukkirli@users.noreply.github.com>
2022-01-17 23:58:12 +0300

    Add a project plan

---
+++ script.js
@@ -0,0 +1,10 @@
+/*
+    /Yapılacalaklar/
+    - Reset css eklenecek
+    - Form tasarımı yapılacak
+    - Temel todo işlemleri(ekleme, silme, tikleme) eklenecek
+    - Tarih eklenecek(moment.js)
+    - Completed kısmı eklenecek
+    - Local storage
+    - DB kaydı(kişisel kullanım için)
+*/
