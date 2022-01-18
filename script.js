908370addeaaa569139d3bda99dde6f6236e5117
ekucukkirli <56414732+ekucukkirli@users.noreply.github.com>
2022-01-18 17:08:13 +0300

    add input

--- script.js
+++ script.js
@@ -1,10 +1,38 @@
 /*
     /Yapılacalaklar/
     - Reset css eklenecek+
-    - Form tasarımı yapılacak
+    - Form tasarımı yapılacak+
     - Temel todo işlemleri(ekleme, silme, tikleme) eklenecek
     - Tarih eklenecek(moment.js)
     - Completed kısmı eklenecek
     - Local storage
     - DB kaydı(kişisel kullanım için)
 */
+
+document.querySelector('form').addEventListener('submit', handleSubmitForm)
+document.querySelector('.list').addEventListener('click', handleClickDeleteOrCheck)
+
+function handleSubmitForm(e) {
+    e.preventDefault();
+    let input = document.querySelector('.add-input');
+    if(input.value!="")
+        addTodo(input.value);
+    input.value = ''
+}
+
+
+function addTodo(todo){
+    let ul = document.querySelector('ul');
+    let li = document.createElement('li');
+    li.innerHTML = `
+        <div class="card-left">
+            <button name="resolved-btn" class="fas fa-check btn resolved-btn"></button>
+            <div class="card-info">
+                <h4 class="task">${todo}</h4>
+            </div>
+        </div>
+        <button name="delete-btn" class="fas fa-times btn delete-btn"></button>
+    `;
+    li.classList.add('card');
+    ul.appendChild(li)
+}
