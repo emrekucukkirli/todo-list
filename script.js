530b416694df52fe2b495f583bc8fec42e849de0
ekucukkirli <56414732+ekucukkirli@users.noreply.github.com>
2022-01-19 19:59:09 +0300

    add todos actions

--- script.js
+++ script.js
@@ -2,37 +2,57 @@
     /Yapılacalaklar/
     - Reset css eklenecek+
     - Form tasarımı yapılacak+
-    - Temel todo işlemleri(ekleme, silme, tikleme) eklenecek
+    - Temel todo işlemleri(ekleme, silme, tikleme) eklenecek+
     - Tarih eklenecek(moment.js)
     - Completed kısmı eklenecek
     - Local storage
     - DB kaydı(kişisel kullanım için)
 */
+const form = document.querySelector('.input');
+const addInput = document.querySelector('.add-input');
+const todoList = document.querySelector('.list');
 
-document.querySelector('form').addEventListener('submit', handleSubmitForm)
-document.querySelector('.list').addEventListener('click', handleClickDeleteOrCheck)
+eventListeners();
 
-function handleSubmitForm(e) {
-    e.preventDefault();
-    let input = document.querySelector('.add-input');
-    if(input.value!="")
-        addTodo(input.value);
-    input.value = ''
+function eventListeners() {
+    form.addEventListener("submit", addTodo);
+    todoList.addEventListener("click", deleteTodo);
+    todoList.addEventListener("click", resolvedTodo);
 }
-
-
-function addTodo(todo){
-    let ul = document.querySelector('ul');
+function addTodo(e){
+    const newTodo = addInput.value.trim();
+    if(newTodo!=""){
+        addTodoUI(newTodo);
+    }
+    e.preventDefault();
+}
+function deleteTodo(e) {
+    if (e.target.name === "delete-btn") {
+        e.target.parentElement.remove();
+    }
+}
+function resolvedTodo(e) {
+    if (e.target.name === "resolved-btn") {
+        if (e.target.parentElement.parentElement.className !=='card resolved') {
+            e.target.parentElement.parentElement.classList.add('resolved');
+        } else {
+            e.target.parentElement.parentElement.classList.remove('resolved');
+        }
+    }
+}
+function addTodoUI(newTodo) {    
     let li = document.createElement('li');
     li.innerHTML = `
         <div class="card-left">
             <button name="resolved-btn" class="fas fa-check btn resolved-btn"></button>
             <div class="card-info">
-                <h4 class="task">${todo}</h4>
+                <h4 class="task">${newTodo}</h4>
             </div>
         </div>
         <button name="delete-btn" class="fas fa-times btn delete-btn"></button>
     `;
     li.classList.add('card');
-    ul.appendChild(li)
+    todoList.appendChild(li);
+    addInput.value="";
+
 }
