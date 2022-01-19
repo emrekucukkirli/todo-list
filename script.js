a508b96b6990cc458a5aac4ec211c5634d135c54
ekucukkirli <56414732+ekucukkirli@users.noreply.github.com>
2022-01-19 20:14:13 +0300

    add date

--- script.js
+++ script.js
@@ -11,6 +11,7 @@
 const form = document.querySelector('.input');
 const addInput = document.querySelector('.add-input');
 const todoList = document.querySelector('.list');
+const date = document.querySelector('#date');
 
 eventListeners();
 
@@ -54,5 +55,20 @@ function addTodoUI(newTodo) {
     li.classList.add('card');
     todoList.appendChild(li);
     addInput.value="";
-
 }
+
+var today = new Date();
+var dd = today.getDate();
+var mm = today.getMonth()+1; 
+var yyyy = today.getFullYear();
+if(dd<10) 
+{
+    dd='0'+dd;
+} 
+if(mm<10) 
+{
+    mm='0'+mm;
+} 
+today = dd+'.'+mm+'.'+yyyy;
+date.innerHTML=`${today}`;
+
